d5d4974e7b6587611b4d45370c1793f2b0de79d2
SaiDheerajPeketi <saidheerajpeketi@gmail.com>
2024-03-11 23:18:19 +0530

    Add Login Page

--- app/src/main/java/com/invincible/miniproject/MainActivity.kt
+++ app/src/main/java/com/invincible/miniproject/MainActivity.kt
@@ -1,15 +1,39 @@
 package com.invincible.miniproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.invincible.miniproject.ui.theme.MiniProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +41,72 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MiniProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                var userNameInput by remember {
+                    mutableStateOf("")
+                }
+                var password by remember {
+                    mutableStateOf("")
+                }
+                var passwordVisibility by remember { 
+                    mutableStateOf(false) 
+                }
+                val icon = if (passwordVisibility)
+                    painterResource(id = R.drawable.show_star)
+                else
+                    painterResource(id = R.drawable.show_eye)
+
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    TextField(
+                        value = userNameInput,
+                        onValueChange = {
+                            userNameInput = it
+                        },
+                        placeholder = { Text(text = "Username") }
+                    )
+
+                    TextField(
+                        value = password,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                        onValueChange = {
+                            password = it
+                        },
+                        placeholder = { Text(text = "Password") },
+                        trailingIcon = {
+                            IconButton(onClick = {
+                                passwordVisibility = !passwordVisibility
+                            }) {
+                                Icon(
+                                    painter = icon,
+                                    contentDescription = "Visibility Icon"
+                                )
+                            }
+                        },
+                        visualTransformation = if (passwordVisibility) VisualTransformation.None
+                        else PasswordVisualTransformation()
+                    )
+
+                    Button(
+                        onClick = {
+//                           Toast.makeText(this@MainActivity, userNameInput, Toast.LENGTH_LONG).show()
+//                           Toast.makeText(this@MainActivity, password, Toast.LENGTH_LONG).show()
+                        }
+                    ) {
+                        Text(text = "Login")
+                    }
+
+                    OutlinedButton(
+                            onClick = {
+                                val intent: Intent = Intent()
+                            }
+                        ) {
+                            Text(text = "New User?")
+                        }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MiniProjectTheme {
-        Greeting("Android")
     }
 }
